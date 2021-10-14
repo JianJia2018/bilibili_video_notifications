6d9cb3604adc99dcf9d00269157f4573d5cedda1
简佳 <15779599039@163.com>
2021-10-14 14:39:25 +0800

    更新

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { init } from "./dynamic.js"
 import dayjs from "dayjs"
 import "dayjs/locale/zh-cn.js"
+import timezone from "dayjs/plugin/timezone.js"
+dayjs.extend(timezone)
+dayjs().locale("zh-cn")
 // init()
 
-console.log("dayjs_locale1", dayjs.locale())
-dayjs().locale("zh-cn")
-
-console.log("dayjs_locale2", dayjs.locale())
-console.log("dayjs", dayjs().locale("zh-cn").format("YYYY-MM-DD HH:mm"))
+dayjs.tz.setDefault("Asia/Shanghai")
+console.log("dayjs1", dayjs().locale("zh-cn").format("YYYY-MM-DD HH:mm"))
+console.log("dayjs1", dayjs().format("YYYY-MM-DD HH:mm"))
