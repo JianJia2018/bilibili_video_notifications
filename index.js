83e1c78c8945f1316924281c38a186aa7381e055
简佳 <15779599039@163.com>
2021-10-14 14:32:05 +0800

    更新

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { init } from "./dynamic.js"
 import dayjs from "dayjs"
+import "dayjs/locale/zh-cn.js"
 // init()
+
+console.log("dayjs_locale1", dayjs.locale())
+dayjs().locale("zh-cn")
+
+console.log("dayjs_locale2", dayjs.locale())
 console.log("dayjs", dayjs().locale("zh-cn").format("YYYY-MM-DD HH:mm"))
