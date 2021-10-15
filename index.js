6fddc58468c5eb630f1e7088831a2e7aa4fa571e
简佳 <15779599039@163.com>
2021-10-15 09:26:15 +0800

    更新

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { init } from "./dynamic.js"
 // init()
 
-console.log("PUSH_PLUS_TOKEN", process.env?.PUSH_PLUS_TOKEN)
-console.log("COOKIE", process.env?.COOKIE)
+console.log("PUSH_PLUS_TOKEN", process.env.PUSH_PLUS_TOKEN)
+console.log("COOKIE", process.env.COOKIE)
