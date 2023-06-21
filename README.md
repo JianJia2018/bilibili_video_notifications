39bacece402a59f606424da37a403b2570f335f6
简佳 <15779599039@163.com>
2023-06-21 10:09:28 +0800

    更新配置

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # bilibili_video_notifications
 B站关注分组视频通知到手机设备
 
+# 支持通知
+### pushplus+, pushdeer
+
+# 运行方式
+1. ### github actions
+
+
+# GitHub Actions secrets and variables 配置
+路径: `Setting => Security => Actions secrets and variables => Actions => Secrets`
+
+`Secrets`配置参数:
+```js
+COOKIE, // b站COOKIE
+PUSH_KEY, // pushdeer的token
+PUSH_PLUS_TOKEN, // pushplus的token
+UID,  //你的uid
+TAG_ID, //你关注的分组id
+```
+
+
 ## 使用说明
-1. 开启github actions
-2. 密钥中加入密钥COOKIE,PUSH_KEY,PUSH_PLUS_TOKEN. 其中B站COOKIE必传, 其它通知token自己选择.
-3. .github/workflows/main.yml中有定时任务时间, 默认为北京时间 7:45, 可以自己修改. 
+1. 开启`github actions`
+2. 密钥中加入密钥`COOKIE`,`PUSH_KEY`,`PUSH_PLUS_TOKEN`, `UID`, `TAG_ID`. 其中B站`COOKIE`, `UID`, `TAG_ID`必传, 其它通知`token`自己选择.
+3. `.github/workflows/main.yml`中有定时任务时间, 默认为北京时间 `19:45`, 可以自己修改. 
--- dynamic.js
+++ dynamic.js
@@ -13,16 +13,20 @@ dayjs.extend(timezone)
 dayjs().locale("zh-cn")
 dayjs.tz.setDefault("Asia/Shanghai")
 
-let tags = []
-let offsetId = 0
-let list = []
-let page = 1
+let tags = [] // 关注的所有标签分组
+let offsetId = 0 // 翻页最后一条id
+let list = [] // 视频动态列表
+let page = 1 // 页数
 
-const yesterday = new Date(new Date().getTime() - 24.5 * 60 * 60 * 1000).getTime()
-const filter_uid = [429711841] // 要过滤的uid
-const Max_Dynamic_Num = 12 // 同一uid最大视频动态条数
+const yesterday = new Date(new Date().getTime() - 24.5 * 60 * 60 * 1000).getTime() // 获取昨天发送的时间, +0.5小时防止误差
+const filter_uid = [429711841] // 要过滤的uid, 默认过滤: 综艺哔哔机
+const Max_Dynamic_Num = 12 // 同一uid最大视频动态条数,防止官方连发
 
-async function getTest() {
+/**
+ * 递归获取关注的所有up主动态
+ * @returns
+ */
+async function getList() {
   try {
     let { data } = await getDynamicHistory(offsetId)
     console.log(`---获取第${page}页数据---`)
@@ -30,20 +34,25 @@ async function getTest() {
     let endData = data.cards[data.cards.length - 1]
     offsetId = endData.desc.dynamic_id
     if (endData.desc.timestamp * 1000 <= yesterday) {
+      // 获取一天的数据完成
       console.log(`---获取完成---`)
       filterListToTags()
       return
     }
+    // 延时请求翻页,防止风控
     setTimeout(() => {
       page++
-      return getTest()
+      return getList()
     }, 8000)
   } catch (e) {
     //TODO handle the exception
-    pushPlusNotify(`获取动态数据失败`, `${e}`)
+    pushPlusNotify(`获取动态数据失败,请更新COOKIE或重试!`, `${e}`)
   }
 }
 
+/**
+ * 在所有list列表里面过滤你需要的分组tag
+ */
 function filterListToTags() {
   let obj = {}
   let myVideos = list
@@ -70,19 +79,23 @@ function filterListToTags() {
     })
     .filter(x => x)
     .reduce((prev, cur) => {
-      // console.log(obj, cur, prev)s
-      obj[cur.link] ? "" : (obj[cur.link] = true && prev.push(cur)) //_id为每个对象独有的标识，即用来判断去重的标识
+      obj[cur.link] ? "" : (obj[cur.link] = true && prev.push(cur)) //_id为每个对象独有的标识，即用来判断去重的标识. 过滤多个up主的合作视频.
       return prev
     }, [])
 
   console.log("myVideos: ", myVideos)
   if (myVideos.filter(x => filter_uid.includes(x.uid)).length >= Max_Dynamic_Num) {
+    // 超出最大动态数量就过滤掉
     sendVideos(myVideos.filter(x => !filter_uid.includes(x.uid)))
   } else {
     sendVideos(myVideos)
   }
 }
 
+/**
+ * 发送视频动态列表到通知软件. 格式为markdown
+ * @param {*} filterList 过滤后的视频动态列表
+ */
 function sendVideos(filterList) {
   console.log("filterList: ", filterList)
   let list = filterList
@@ -107,17 +120,21 @@ ${header}
 
 ${list}`
   console.log("string: ", string)
+
   pushPlusNotify(`${dayjs.tz().format("MM-DD HH:mm")} B站视频动态`, string, "markdown")
   pushDeerNotify(`${dayjs.tz().format("MM-DD HH:mm")} B站视频动态`, string, "markdown")
 }
 
+/**
+ * 初始化函数
+ */
 async function init() {
   try {
     tags = await getAllTags()
     let { data } = await getDynamicNew()
     list = data.cards
-    offsetId = data.cards[data.cards.length - 1].desc.dynamic_id
-    getTest()
+    offsetId = data.cards[data.cards.length - 1].desc.dynamic_id // 翻页id, 动态的最后一个
+    getList()
   } catch (e) {
     pushPlusNotify(`B站动态发送失败`, e)
   }
