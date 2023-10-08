156c3ae870fe4a5ae37639f544da3c5e038bddc6
JianJia2018 <39438074+JianJia2018@users.noreply.github.com>
2023-10-08 10:52:41 +0800

    更新

--- pushplus.js
+++ pushplus.js
@@ -29,6 +29,7 @@ function pushPlusNotify(text, desp = "", template = "html") {
   return new Promise(resolve => {
     if (PUSH_PLUS_TOKEN) {
       if (template === "html") {
+        console.log(desp)
         desp = desp?.replace(/[\n\r]/g, "<br>") // 默认为html, 不支持plaintext
       }
       const body = {
