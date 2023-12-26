d4203a253087210f08649249e86e7f3ccd165203
简佳 <15779599039@163.com>
2023-12-26 09:19:24 +0800

    更新青龙脚本头

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { init } from "./dynamic.js"
-/**
-  cron "55 7 * * *" B站视频动态推送.js, tag=B站视频动态推送
+/*
+cron "55 7 * * *" B站视频动态推送.js, tag=B站视频动态推送
  */
-
+import { init } from "./dynamic.js"
 !(async () => {
   // 代码开始
   init()
